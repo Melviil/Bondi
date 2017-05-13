caef3f6867938eaba9cec6572db2110ef476c4be
Melvil <melvildonnart3@gmail.com>
2017-05-13 12:09:22 +0200

    fix make it better

--- index.php
+++ index.php
@@ -1,1 +1,48 @@
-<?php header( 'Location: map/index.html' ) ;  ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>map3</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <link rel="stylesheet" href="http://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.css" />
+  <script src="http://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.js"></script>
+  <script>
+      var layer;
+    function init() {
+        var bounds = new L.LatLngBounds(
+            new L.LatLng(-85.051129, -180.000000),
+            new L.LatLng(85.051129, 179.989213));
+        var map = L.map('map').fitBounds(bounds);
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+        var options = {
+            minZoom: 0,
+            maxZoom: 4,
+            opacity: 1.0,
+            tms: false
+        };
+        layer = L.tileLayer('{z}/{x}/{y}.png', options).addTo(map);
+    
+    var greenIcon = L.icon({
+    iconUrl: '../pin/leaf-green.png',
+    shadowUrl: '../pin/leaf-shadow.png',
+
+    iconSize:     [60, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+  L.marker([45.151394, 1.545289], {icon: greenIcon}).addTo(map) // chez moi
+
+}
+  </script>
+  <style>
+    body { margin:0; padding:0; }
+    #map { position:absolute; top:0; bottom:0; width:100%; z-index: 1; }
+    #slider{ position: absolute; top: 10px; right: 10px; z-index: 5; }
+  </style>
+</head>
+<body onload="init()">
+<div id="map"></div>
+<input id="slider" type="range" min="0" max="1" step="0.1" value="1" oninput="layer.setOpacity(this.value)">
+</body>
+</html>
