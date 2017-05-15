60ed6a8134320f415c1a6604964b54060409d484
Melvil <melvildonnart3@gmail.com>
2017-05-15 18:23:33 +0200

    dezoom marche

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html >
+  	<head>
+	   <title>BONDI</title>
+	    <link rel="stylesheet"  type="text/css" href="app.css"></script>
+	    <script type="text/javascript" src="creationMap.js"></script>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	    <link rel="stylesheet" href="http://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.css" />
+	    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD1xK9LoXfFhnUvIopgzYSNxioyhHDSbuU"></script>
+	    <script src="http://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.js"></script>
+	    <script type="text/javascript" src="bootbox.min.js"></script>
+	     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript" src="bootstrap.min.js"></script>
+     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+ 	</head>
+ 	<style>
+ 	.leaflet-control-attribution.leaflet-control {
+    display: none !important;
+}
+ 	.pic{
+ 		max-width: 400px;
+ 		max-height: 400px;
+ 	}
+ 	.
+ 	.post p{
+ 		font-family: cursive;
+ 	}
+ 	</style>
+ 	<body onload="initMap()">
+<div id="map" ></div>
+ 	 </body>
+</html>
+
