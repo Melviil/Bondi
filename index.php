4e016221eaea7c6f61a27ba67af4f6a12e1dffe3
Melvil <melvildonnart3@gmail.com>
2017-05-16 09:19:39 +0200

    design & css

--- index.php
+++ index.php
@@ -2,42 +2,25 @@
 <html >
   	<head>
 	   <title>BONDI</title>
-	   <link rel="stylesheet"  type="text/css" href="app.css"></script>
+	    <link rel="stylesheet"  type="text/css" href="app.css"></script>
 	    <script type="text/javascript" src="creationMap.js"></script>
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	    <link rel="stylesheet" href="https://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.css" />
-	    <script src="https://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
+	    <link rel="stylesheet" href="leaflet/leaflet.css" />
 	    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD1xK9LoXfFhnUvIopgzYSNxioyhHDSbuU"></script>
-	    <script src="https://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.js"></script>
+	    <script src="leaflet/leaflet.js"></script>
 	    <script type="text/javascript" src="bootbox.min.js"></script>
-	     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script type="text/javascript" src="bootstrap.min.js"></script>
-     <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+     	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+     	<link rel="stylesheet" type="text/css" href="map.css">
 
  	</head>
- 	<style>
- 	.leaflet-control-attribution.leaflet-control {
-    display: none !important;
-}
- 	.pic{
- 		max-width: 400px;
- 		max-height: 400px;
- 	}
- 	.
- 	.post p{
- 		font-family: cursive;
- 	}
- 	#addmarker{
- 		position: absolute;
-    right: 0;
-    max-width: 100px;
-    top: 50%;
- 	}
- 	</style>
+ 	
  	<body onload="initMap()">
-<div id="map" >
-<input  type="image" id="addmarker" class="leaflet-control-zoom leaflet-bar leaflet-control" src="../bondi/leaflet/addMarker.png" alt="Add Marker" />
-</div>
+<div id="map" ></div>
+<input  type="image" id="addmarker" class="leaflet-control-zoom leaflet-bar leaflet-control" src="../bondi/custom/add.png" alt="Add Marker" />
+<input  type="image" id="admin" class="leaflet-control-zoom leaflet-bar leaflet-control" src="../bondi/custom/admin.png" alt="Admin page " />
+<input  type="image" id="about" class="leaflet-control-zoom leaflet-bar leaflet-control" src="../bondi/custom/about.png" alt="Admin page " />
+
  	 </body>
 </html>
 
