7b710facaf0afc5385cba8943ef2a95cd1931867
Melvil <melvildonnart3@gmail.com>
2017-05-18 17:19:49 +0200

    cors

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors');
-
 
 
 //connection database local
@@ -21,7 +19,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.use(cors()); //fix cors problem
+
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
@@ -46,6 +44,26 @@ app.use(function(req,res,next){
 
 app.use('/', index);
 app.use('/users', users);
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 
