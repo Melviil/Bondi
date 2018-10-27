2124969e839c0c4b7b11219d089d2c633aff4d33
Melvil Donnart <melvildonnart3@gmail.com>
2018-10-27 22:37:43 +0200

    final correction

--- public/javascripts/creationMap.js
+++ public/javascripts/creationMap.js
@@ -61,7 +61,7 @@ function initMap() {
   map.on('click', function(e){
     // var x = document.getElementsByClassName("leaflet-popup-content-wrapper");
     // if ( x.length == 0 ){ // if no pop up
-      // newMarkerMap(e); // add marker
+       newMarkerMap(e); // add marker
     // }else{ //if at least one popup is open
       map.closePopup(); // otherwise close popup
     // }
